619ba18a911533e78b3cb822df4bc685c1833404
dave <davefinlay@gmail.com>
2014-07-08 11:37:58 -0700

    hello world go program

---
+++ go/src/hello.go
@@ -0,0 +1,16 @@
+/**
+ * User: dave
+ */
+package main
+
+import (
+       "fmt"
+)
+
+func main() {
+	sum := 0
+	for i := 0; i < 10; i++ {
+		sum += i
+	}
+    fmt.Printf("hello, world: %v\n", sum)
+}
