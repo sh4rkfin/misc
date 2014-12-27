712c0677d6213220af6cee0d20e697bffd843fcf
dave <davefinlay@gmail.com>
2014-12-27 17:13:58 -0600

    Add dimension_count(), get_arg_max() and matrix collapsing routines.

--- python/util.py
+++ python/util.py
@@ -20,7 +20,7 @@ def ensure_has_capacity(array, size, default_value_function=None):
 
 def is_iterable(obj):
     return hasattr(obj, '__iter__') or \
-           hasattr(obj, '__len__') and hasattr(obj, '__getitem__')
+        hasattr(obj, '__len__') and hasattr(obj, '__getitem__')
 
 
 def sum_off_diagonal(twod_array):
@@ -30,3 +30,53 @@ def sum_off_diagonal(twod_array):
             if i != j:
                 result += y
     return result
+
+
+def dimension_count(m):
+    current = m
+    result = 0
+    while isinstance(current, list):
+        result += 1
+        if len(current) == 0:
+            break
+        current = current[0]
+    return result
+
+
+def add_to(m1, m2):
+    if m1 is None:
+        m1 = make_zero_matrix_of_same_dimension(m2)
+    if m2 is None:
+        return m1
+    l1, l2 = len(m1), len(m2)
+    if l1 > l2:
+        raise ValueError("lengths not compatible")
+    is_list = None
+    for i in xrange(l1):
+        if is_list is None:
+            is_list = isinstance(m1[i], list)
+        if is_list:
+            add_to(m1[i], m2[i])
+        else:
+            m1[i] += m2[i]
+    return m1
+
+
+def make_zero_matrix_of_same_dimension(m):
+    return [make_zero_matrix_of_same_dimension(v) for v in m] if isinstance(m, list) else 0
+
+
+def accumulate(collection, accumulator, initial_value=None):
+    result = initial_value
+    for x in collection:
+        result = accumulator(result, x)
+    return result
+
+
+def arg_max(lst, key):
+    max_val, result = None, None
+    for a in lst:
+        if max_val is None or max_val < key(a):
+            max_val, result = key(a), a
+    return result
+
--- python/util_test.py
+++ python/util_test.py
@@ -1,11 +1,31 @@
 #!/usr/bin/env python
 import unittest
 
+import util
+
 
 class UtilTestCase(unittest.TestCase):
     def test_tuple_len(self):
         tuple = ('one', 2, 3.0)
         self.assertEqual(len(tuple), 3)
+
+    def test_tuple_access(self):
+        tuple = ('one', 2, 3.0)
+        self.assertEqual(tuple[0], 'one')
+        self.assertEqual(tuple[1], 2)
+
+    def test_tuple_comparison(self):
+        t1 = ('one', 1)
+        t2 = ('two', 2)
+        t3 = ('three', 3)
+        t4 = ('four', 4)
+        ts = [t2, t1, t3, t4]
+        ts.sort()
+        self.assertEqual(ts[0], t4)
+        self.assertEqual(ts[1], t1)
+        self.assertEqual(ts[2], t3)
+        self.assertEqual(ts[3], t2)
+        #print ts
 
     def test_break_continue(self):
         count = 0
@@ -30,6 +50,69 @@ class UtilTestCase(unittest.TestCase):
         self.assertEqual(a.bar, 'baz')
         a.__dict__['2d'] = 'hit'
         # invalid syntax: a.2d
+
+    def test_basic_collapse(self):
+        l = [1, 2, 3]
+        result = util.accumulate(l, lambda x, y: x+y, 0)
+        print "sum:", result
+        self.assertEqual(6, result)
+
+    def test_add_matrices(self):
+        m1 = [[1 for _ in range(4)] for _ in range(3)]
+        m2 = [[2 for _ in range(4)] for _ in range(5)]
+        util.add_to(m1, m2)
+        print m1
+        for a in m1:
+            for b in a:
+                self.assertEqual(b, 3)
+
+    def test_make_zero_matrix(self):
+        m1 = [[1 for _ in range(4)] for _ in range(3)]
+        m2 = util.make_zero_matrix_of_same_dimension(m1)
+        self.assertEqual(len(m2), 3)
+        for a in m2:
+            self.assertEqual(len(a), 4)
+            for b in a:
+                self.assertEqual(b, 0)
+
+    def test_accumulate_matrices(self):
+        m1 = [[1 for _ in range(4)] for _ in range(3)]
+        m2 = [[2 for _ in range(4)] for _ in range(5)]
+        m = [m1, m2]
+
+        def add_to(first, second):
+            if first is None:
+                first = util.make_zero_matrix_of_same_dimension(second)
+            util.add_to(first, second)
+            return first
+
+        result = util.accumulate(m, util.add_to)
+        self.assertEqual(len(result), 3)
+        for a in result:
+            self.assertEqual(len(a), 4)
+            for b in a:
+                self.assertEqual(b, 3)
+
+    def test_dimension_count(self):
+        m1 = [[1 for _ in range(4)] for _ in range(3)]
+        m2 = [[2 for _ in range(4)] for _ in range(5)]
+        self.assertEqual(util.dimension_count(m1), 2)
+        self.assertEqual(util.dimension_count(m2), 2)
+        m = [m1, m2]
+        self.assertEqual(util.dimension_count(m), 3)
+        self.assertEqual(util.dimension_count([]), 1)
+        self.assertEqual(util.dimension_count([[]]), 2)
+        self.assertEqual(util.dimension_count([[[]]]), 3)
+
+    def test_arg_max(self):
+        t1 = ('one', 1)
+        t2 = ('two', 2)
+        t3 = ('three', 3)
+        t4 = ('four', 4)
+        ts = [t2, t1, t3, t4]
+        arg_max = util.arg_max(ts, lambda x: x[0])
+        print "arg max:", arg_max
+        self.assertEqual(arg_max, t2)
 
 if __name__ == '__main__':
     unittest.main()
