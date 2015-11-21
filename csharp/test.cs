d1ffde6b52e3fded59aace2ba8b323d4cda5b64d
dave <davefinlay@gmail.com>
2015-11-21 11:56:59 -0800

    extremely basic csharp test file

---
+++ csharp/test.cs
@@ -0,0 +1,17 @@
+/*
+    Starter C# file. Can be run on Mac OSX using mono (download with homebrew.)
+    To run:
+      1) mcs test.cs
+      2) mono test.exe
+
+*/
+using System.Collections.Generic;
+class Node {
+    List<Node> children;
+
+    static public void Main (string[] args)
+    {
+	System.Console.WriteLine("Hello, World!");
+    }
+
+};
